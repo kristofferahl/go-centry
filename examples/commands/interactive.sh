23fc39bd3d610bffd8e33b1f8415fb2546ec442f
Kristoffer Ahl <git@77dynamite.com>
2018-08-29 16:29:46 +0200

    - Cleaned up interactive example command.

--- examples/commands/interactive.sh
+++ examples/commands/interactive.sh
@@ -1,28 +1,8 @@
 #!/usr/bin/env bash
 
-up:packages () {
-  echo "up:packages ($*)"
-}
-
-up:modules () {
-  echo "up:modules ($*)"
-}
-
 interactive () {
   echo "interactive ($*)"
   echo -n "Enter your name: "
   read -r name
   echo "Thanks ${name:?}!"
-}
-
-down:packages () {
-  echo "down:packages ($*)"
-}
-
-down:modules () {
-  echo "down:modules ($*)"
-}
-
-down () {
-  echo "up ($*)"
 }
