f5267b79aaeb304a60943ffec4802a38c3289606
Kristoffer Ahl <git@77dynamite.com>
2019-06-01 11:04:10 +0200

    - Updated examples.

--- examples/centry/scripts/init.sh
+++ examples/centry/scripts/init.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+init() {
+  if [[ "${DEBUG}" == true ]]; then
+    set -x
+  fi
+
+  case "${SORTED}" in
+    asc) SORTED='sort' ;;
+    desc) SORTED='sort -r' ;;
+    *) SORTED='cat' ;;
+  esac
+}
+
+init "$@"
